9514717e1e24bd5e0e5011b81399d751edc8b48e
Elvis Estevan <elvis.estevan@soaexpert.com.br>
2019-01-28 13:36:21 -0200

    Day 19 - Interfaces

---
+++ 19-interfaces.py
@@ -0,0 +1,22 @@
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        soma = 0
+        for i in range(n):
+            v = i + 1
+            resto = n % v
+            if (resto == 0):
+                soma += v
+
+        return soma
+
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
