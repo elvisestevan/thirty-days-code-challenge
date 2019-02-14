a36accb7c89675dfe628bce041465d49c018e35a
Elvis Estevan <elvis.estevan@soaexpert.com.br>
2019-02-14 13:09:00 -0200

    Day 24 - More Linked Lists

---
+++ 24-more-linked-lists.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        currentNode = head
+        
+        while not currentNode.next == None:            
+            if currentNode.data == currentNode.next.data:
+                currentNode.next = currentNode.next.next
+            else:
+                currentNode = currentNode.next
+
+        return head
+
+
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
